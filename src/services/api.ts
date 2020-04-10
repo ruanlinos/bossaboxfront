7a84ee491609e0d12ecdc23f6e8d0b1dc5c6d18c
Ruan Linos Alves <ruandeveloper@hotmail.com>
2020-04-10 17:32:05 -0300

    fix: fixed services/api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://bossaboxapi.herokuapp.com' : 'http://localhost:3000';
 
 const api = axios.create({
-  baseURL: 'https://bossaboxapi.herokuapp.com'
+  baseURL,
 });
 
 export default api;
